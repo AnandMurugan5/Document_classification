57fe71cfc93c4421e3d25c92de3c05b3079d5041
Anand <anandmurugan2499@gmail.com>
2024-01-16 15:26:31 +0530

    fix issue

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import os
 from email_extraction import EmailData
-from model_training import ContentPrediction, DocumentPrediction
-
+from model_training import DocumentPrediction,ContentPrediction
 # Function to login and store session state
 def login(email, password):
     try:
